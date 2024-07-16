415ff05e40e2e9e8a8918276a2365418492f7e91
Eyşan Tekinsoy <eysantekinsoy@hotmail.com>
2024-07-16 16:42:51 +0300

    butonlara iconlar eklendi

--- Uygulama/REACT/recipe-app/src/components/Card.jsx
+++ Uygulama/REACT/recipe-app/src/components/Card.jsx
@@ -2,13 +2,16 @@ import React, { useContext } from 'react'
 import '../assets/style/card.scss'
 import Food from '../assets/img/dontfood.png'
 import DataContext from '../context/DataContext'
+import { FaRegTrashAlt } from "react-icons/fa";
+import { MdOutlineEdit } from "react-icons/md";
+
 
 const Card = ({food}) => {
   const {foodDelete,cardDuzenle} = useContext(DataContext);
   return (
         <div className='card'>
-          <button onClick={()=>foodDelete(food.id)} className='delete'>SİL</button>
-          <button onClick={()=>cardDuzenle(food.id)} className='edit'>DÜZENLE</button>
+          <button onClick={()=>foodDelete(food.id)} className='delete'><FaRegTrashAlt size={11}/> SİL</button>
+          <button onClick={()=>cardDuzenle(food.id)} className='edit'><MdOutlineEdit/>DÜZENLE</button>
           <img src={food.image?food.image:Food} alt={food.title+"_kapak"} />
           <div className="card-body">
           <h4>{food.title}</h4>
